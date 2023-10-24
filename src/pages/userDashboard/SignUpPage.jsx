63aa8fb19859e7a184e9fe70d26efb6af2e343d7
ebenezersakyi <ebensakyi0@gmail.com>
2023-10-24 19:42:21 +0000

    comsole

--- src/pages/userDashboard/SignUpPage.jsx
+++ src/pages/userDashboard/SignUpPage.jsx
@@ -51,6 +51,7 @@ const SignUpPage = () => {
     validationSchema,
     // validate: (values) => {},
     onSubmit: (values) => {
+      console.log("starting");
       createSignUpApproval();
     },
   });
